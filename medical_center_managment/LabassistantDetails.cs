8a5ff31d9d85dd333007ec8e5923d5dbc8f95173
GihanSisiraKumara <gihan.sisirakumara2000@gmail.com>
2024-05-12 23:37:04 +0800

    Create LabAssistantDetails design

---
+++ medical_center_managment/LabassistantDetails.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace medical_center_managment
+{
+    public partial class LabassistantDetails : Form
+    {
+        public LabassistantDetails()
+        {
+            InitializeComponent();
+        }
+    }
+}
