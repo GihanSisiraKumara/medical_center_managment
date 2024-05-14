d34421db00dc0ec5ca3b7abb2342654e3a844b8e
GihanSisiraKumara <gihan.sisirakumara2000@gmail.com>
2024-05-15 02:00:47 +0800

    connect to backend to LabassistantDetails page

--- medical_center_managment/LabassistantDetails.Designer.cs
+++ medical_center_managment/LabassistantDetails.Designer.cs
@@ -41,22 +41,22 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.textBox5 = new System.Windows.Forms.TextBox();
-            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.idtb = new System.Windows.Forms.TextBox();
+            this.firsttb = new System.Windows.Forms.TextBox();
+            this.secondb = new System.Windows.Forms.TextBox();
+            this.specialtb = new System.Windows.Forms.TextBox();
+            this.emailtb = new System.Windows.Forms.TextBox();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.Test = new System.Windows.Forms.Button();
             this.button14 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.gendertb = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -167,9 +167,9 @@
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(37, 175);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(93, 25);
+            this.label1.Size = new System.Drawing.Size(116, 25);
             this.label1.TabIndex = 35;
-            this.label1.Text = "Doctar ID";
+            this.label1.Text = "Assistant ID";
             // 
             // label2
             // 
@@ -191,21 +191,11 @@
             this.label3.TabIndex = 37;
             this.label3.Text = "Second Name";
             // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(37, 353);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(139, 25);
-            this.label4.TabIndex = 38;
-            this.label4.Text = "Hospital Name";
-            // 
             // label6
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(37, 408);
+            this.label6.Location = new System.Drawing.Point(37, 403);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(77, 25);
             this.label6.TabIndex = 39;
@@ -215,7 +205,7 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(37, 467);
+            this.label5.Location = new System.Drawing.Point(37, 462);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(60, 25);
             this.label5.TabIndex = 40;
@@ -230,6 +220,7 @@
             this.button1.TabIndex = 41;
             this.button1.Text = "Insert";
             this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button3
             // 
@@ -240,6 +231,7 @@
             this.button3.TabIndex = 42;
             this.button3.Text = "Update";
             this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
@@ -250,6 +242,7 @@
             this.button4.TabIndex = 43;
             this.button4.Text = "Delect";
             this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // button2
             // 
@@ -260,48 +253,42 @@
             this.button2.TabIndex = 44;
             this.button2.Text = "Search";
             this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
-            // textBox1
+            // idtb
             // 
-            this.textBox1.Location = new System.Drawing.Point(195, 175);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(135, 22);
-            this.textBox1.TabIndex = 45;
+            this.idtb.Location = new System.Drawing.Point(195, 175);
+            this.idtb.Name = "idtb";
+            this.idtb.Size = new System.Drawing.Size(135, 22);
+            this.idtb.TabIndex = 45;
             // 
-            // textBox2
+            // firsttb
             // 
-            this.textBox2.Location = new System.Drawing.Point(195, 236);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(135, 22);
-            this.textBox2.TabIndex = 46;
+            this.firsttb.Location = new System.Drawing.Point(195, 236);
+            this.firsttb.Name = "firsttb";
+            this.firsttb.Size = new System.Drawing.Size(135, 22);
+            this.firsttb.TabIndex = 46;
             // 
-            // textBox3
+            // secondb
             // 
-            this.textBox3.Location = new System.Drawing.Point(195, 296);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(135, 22);
-            this.textBox3.TabIndex = 47;
+            this.secondb.Location = new System.Drawing.Point(195, 296);
+            this.secondb.Name = "secondb";
+            this.secondb.Size = new System.Drawing.Size(135, 22);
+            this.secondb.TabIndex = 47;
             // 
-            // textBox4
+            // specialtb
             // 
-            this.textBox4.Location = new System.Drawing.Point(195, 356);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(135, 22);
-            this.textBox4.TabIndex = 48;
+            this.specialtb.Location = new System.Drawing.Point(195, 403);
+            this.specialtb.Name = "specialtb";
+            this.specialtb.Size = new System.Drawing.Size(135, 22);
+            this.specialtb.TabIndex = 49;
             // 
-            // textBox5
+            // emailtb
             // 
-            this.textBox5.Location = new System.Drawing.Point(195, 412);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(135, 22);
-            this.textBox5.TabIndex = 49;
-            // 
-            // textBox6
-            // 
-            this.textBox6.Location = new System.Drawing.Point(195, 467);
-            this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(135, 22);
-            this.textBox6.TabIndex = 50;
+            this.emailtb.Location = new System.Drawing.Point(195, 462);
+            this.emailtb.Name = "emailtb";
+            this.emailtb.Size = new System.Drawing.Size(135, 22);
+            this.emailtb.TabIndex = 50;
             // 
             // dataGridView1
             // 
@@ -333,27 +320,44 @@
             this.button14.Text = "LB Assis";
             this.button14.UseVisualStyleBackColor = false;
             // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(37, 351);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(77, 25);
+            this.label4.TabIndex = 67;
+            this.label4.Text = "Gender";
+            // 
+            // gendertb
+            // 
+            this.gendertb.Location = new System.Drawing.Point(195, 355);
+            this.gendertb.Name = "gendertb";
+            this.gendertb.Size = new System.Drawing.Size(135, 22);
+            this.gendertb.TabIndex = 68;
+            // 
             // LabassistantDetails
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1280, 587);
+            this.Controls.Add(this.gendertb);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.button14);
             this.Controls.Add(this.Test);
             this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.textBox6);
-            this.Controls.Add(this.textBox5);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.emailtb);
+            this.Controls.Add(this.specialtb);
+            this.Controls.Add(this.secondb);
+            this.Controls.Add(this.firsttb);
+            this.Controls.Add(this.idtb);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label6);
-            this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -369,6 +373,7 @@
             this.Controls.Add(this.label8);
             this.Name = "LabassistantDetails";
             this.Text = "Lab Assistant";
+            this.Load += new System.EventHandler(this.LabassistantDetails_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -390,21 +395,21 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.TextBox textBox5;
-        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox idtb;
+        private System.Windows.Forms.TextBox firsttb;
+        private System.Windows.Forms.TextBox secondb;
+        private System.Windows.Forms.TextBox specialtb;
+        private System.Windows.Forms.TextBox emailtb;
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Button Test;
         private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox gendertb;
     }
 }
--- medical_center_managment/LabassistantDetails.cs
+++ medical_center_managment/LabassistantDetails.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,170 @@ namespace medical_center_managment
 {
     public partial class LabassistantDetails : Form
     {
+        string connectionString = @"Data Source=LAPTOP-4VMD8P7I;Initial Catalog=medical_center;Integrated Security=True;";
         public LabassistantDetails()
         {
             InitializeComponent();
+        }
+        public void display_data()
+        {
+            try
+            {
+                using (SqlConnection sqlCon = new SqlConnection(connectionString))
+                {
+                    sqlCon.Open();
+                    SqlCommand cmd = sqlCon.CreateCommand();
+                    cmd.CommandType = CommandType.Text;
+                    cmd.CommandText = "select * from Labassistant";
+                    cmd.ExecuteNonQuery();
+                    DataTable dataTable = new DataTable();
+                    SqlDataAdapter da = new SqlDataAdapter(cmd);
+                    da.Fill(dataTable);
+                    dataGridView1.DataSource = dataTable;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occurred: " + ex.Message);
+            }
+        }
+
+        private void LabassistantDetails_Load(object sender, EventArgs e)
+        {
+            display_data();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (SqlConnection sqlCon = new SqlConnection(connectionString))
+                {
+                    sqlCon.Open();
+                    SqlCommand sqlCmd = new SqlCommand("insertlabassitant", sqlCon);
+                    sqlCmd.CommandType = CommandType.StoredProcedure;
+                    sqlCmd.Parameters.AddWithValue("@assisid", idtb.Text.Trim());
+                    sqlCmd.Parameters.AddWithValue("@firstname", firsttb.Text.Trim());
+                    sqlCmd.Parameters.AddWithValue("@secondname", secondb.Text.Trim());
+                    sqlCmd.Parameters.AddWithValue("@gender", gendertb.Text.Trim());
+                    sqlCmd.Parameters.AddWithValue("@qualification", specialtb.Text.Trim());
+                    sqlCmd.Parameters.AddWithValue("@email", emailtb.Text.Trim());
+                   
+                    sqlCmd.ExecuteNonQuery();
+                    display_data();
+                    idtb.Text = "";
+                   
+                    MessageBox.Show("Register is successfull !");
+                    Clear();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occurred: " + ex.Message);
+            }
+        }
+        void Clear()
+        {
+            idtb.Text = firsttb.Text = secondb.Text = gendertb.Text = specialtb.Text = emailtb.Text =  "";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (SqlConnection sqlCon = new SqlConnection(connectionString))
+                {
+                    sqlCon.Open();
+                    SqlCommand cmd = sqlCon.CreateCommand();
+                    cmd.CommandType = CommandType.Text;
+                    cmd.CommandText = "UPDATE Labassistant SET FirstName = @firstname, SecondName = @secondname, Gender = @gender, Qualification = @qualification, Email = @email WHERE LabassistantID = @assisid";
+                    cmd.Parameters.AddWithValue("@assisid", idtb.Text.Trim());
+                    cmd.Parameters.AddWithValue("@firstname", firsttb.Text.Trim());
+                    cmd.Parameters.AddWithValue("@secondname", secondb.Text.Trim());
+                    cmd.Parameters.AddWithValue("@gender", gendertb.Text.Trim());
+                    cmd.Parameters.AddWithValue("@qualification", specialtb.Text.Trim());
+                    cmd.Parameters.AddWithValue("@email", emailtb.Text.Trim());
+                   
+                    cmd.ExecuteNonQuery();
+                    display_data();
+                    MessageBox.Show("Update is successful!");
+                    Clear();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occurred: " + ex.Message);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (SqlConnection sqlCon = new SqlConnection(connectionString))
+                {
+                    sqlCon.Open();
+                    SqlCommand cmd = sqlCon.CreateCommand();
+                    cmd.CommandType = CommandType.Text;
+                    cmd.CommandText = "DELETE FROM Labassistant WHERE LabassistantID = '" + idtb.Text + "'";
+
+                    cmd.ExecuteNonQuery();
+                    connectionString.Clone();
+                    display_data();
+                    MessageBox.Show("Delect is succesfull !");
+
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occurred: " + ex.Message);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (SqlConnection sqlCon = new SqlConnection(connectionString))
+                {
+                    sqlCon.Open();
+                    SqlCommand cmd = sqlCon.CreateCommand();
+                    cmd.CommandType = CommandType.Text;
+                    cmd.CommandText = "SELECT * FROM Labassistant WHERE LabassistantID = @assisid ";
+                    cmd.Parameters.AddWithValue("@assisid", idtb.Text.Trim());
+                   
+                    cmd.ExecuteNonQuery();
+                    DataTable dataTable = new DataTable();
+                    SqlDataAdapter da = new SqlDataAdapter(cmd);
+                    da.Fill(dataTable);
+                    dataGridView1.DataSource = dataTable;
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    if (reader.Read())
+                    {
+                        idtb.Text = reader["LabassistantID"].ToString();
+                        firsttb.Text = reader["FirstName"].ToString();
+                        secondb.Text = reader["SecondName"].ToString();
+                        gendertb.Text = reader["Gender"].ToString();
+                        specialtb.Text = reader["Qualification"].ToString();
+                        emailtb.Text = reader["Email"].ToString();
+                        
+                    }
+                    else
+                    {
+                        MessageBox.Show("No matching records found.");
+                    }
+
+                    reader.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occurred: " + ex.Message);
+            }
         }
     }
 }
